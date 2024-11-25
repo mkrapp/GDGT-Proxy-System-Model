cf05337e2be12dd39e6e03be9aa0ee3fa2019a2b
Mario Krapp <mariokrapp@gmail.com>
2024-11-26 12:37:47 +1300

    Add Tierney 2015 dataset

--- src/data/GDGT_datasets.py
+++ src/data/GDGT_datasets.py
@@ -16,48 +16,107 @@ AGE       = "Age (Ma)"
 LATITUDE  = "Latitude"
 LONGITUDE = "Longitude"
 SST       = "SST"
+GDGT      = ["GDGT-0", "GDGT-1", "GDGT-2", "GDGT-3", "Crenarchaeol", "Cren'"]
 
 @validate_params(
-    {"return_X_y": ["boolean"], "as_frame": ["boolean"]},
-    prefer_skip_nested_validation=True,
-)
+        {"return_X_y": ["boolean"], "as_frame": ["boolean"]},
+        prefer_skip_nested_validation=True,
+        )
+def load_tierney2015(*, return_X_y=False, as_frame=False):
+    data_file_name = "../data/external/tierney2015tex86.txt"
+
+    df = pd.read_csv(data_file_name,sep="\t",comment="#",na_values=[-999])
+
+    # Mapping of column names
+    col_map = {
+            "fGDGT_0": "GDGT-0",
+            "fGDGT_1": "GDGT-1",
+            "fGDGT_2": "GDGT-2",
+            "fGDGT_3": "GDGT-3",
+            "fGDGT_cren": "Crenarchaeol",
+            "fGDGT_cren'": "Cren'",
+            "longitude": LONGITUDE,
+            "latitude": LATITUDE,
+            "WOA09_SST": SST
+            }
+
+    df = df.rename(columns=col_map)
+    print(df)
+
+    idx = ~(df[GDGT]==0).any(axis=1)
+
+    columns = GDGT+[SST,LONGITUDE,LATITUDE]
+    df = df[columns].loc[idx]
+
+    df = df.dropna(axis=0)
+
+    df = df.astype(np.float64)
+
+    feature_columns = GDGT+[LONGITUDE,LATITUDE]
+
+    data, target, feature_names, target_names = df[feature_columns].values, df[SST].values, feature_columns, [SST]
+
+    fdescr = read_text(DESCR_MODULE, "tierney2015.rst")
+
+    frame = None
+    target_columns = [
+            SST,
+            ]
+    if as_frame:
+        frame, data, target = _convert_data_dataframe(
+                "load_tierney2015", data, target, feature_names, target_columns
+                )
+
+    if return_X_y:
+        return data, target
+
+    return Bunch(
+            data=data,
+            target=target,
+            frame=frame,
+            target_names=target_names,
+            DESCR=fdescr,
+            feature_names=feature_names,
+            filename=data_file_name,
+            data_module=DATA_MODULE,
+            )
+
+@validate_params(
+        {"return_X_y": ["boolean"], "as_frame": ["boolean"]},
+        prefer_skip_nested_validation=True,
+        )
 def load_rattanasriampaipong2022(*, modern=True, return_X_y=False, as_frame=False):
 
     data_file_name = "../data/external/pnas.2123193119.sd02.xlsx"
 
     df = pd.read_excel(data_file_name,index_col=0)
-    
-    # sst_name = "Sea Surface Temp"
-    sst_name = "WOA18_decav_SST"
-    
+
     # Mapping of column names
     col_map = {
-        "cal_frac_1302": "GDGT-0",
-        "cal_frac_1300": "GDGT-1",
-        "cal_frac_1298": "GDGT-2",
-        "cal_frac_1296": "GDGT-3",
-        "cal_frac_1292": "Crenarchaeol",
-        "cal_frac_1292_iso": "Cren'",
-        sst_name: SST
-        }
-    
+            "cal_frac_1302": "GDGT-0",
+            "cal_frac_1300": "GDGT-1",
+            "cal_frac_1298": "GDGT-2",
+            "cal_frac_1296": "GDGT-3",
+            "cal_frac_1292": "Crenarchaeol",
+            "cal_frac_1292_iso": "Cren'",
+            "WOA18_decav_SST": SST
+            }
+
     age_str = "Modern" if modern else "Ancient"
     class_str = "sediment-totalGDGTs" if modern else "paleoGDGTs"
-    
+
     if not modern:
         df[LATITUDE] = df["paleolat"].values
         df[LONGITUDE] = df["paleolon"].values
-    
+
     df = df[np.logical_and(df["dataType_level0"] == age_str,df["lipidClass"] == class_str)]
     df[AGE] = df["sampleAge"].values
 
     df = df.rename(columns=col_map)
 
-    compound_names = df.columns[19:25]
+    idx = ~(df[GDGT]==0).any(axis=1)
 
-    idx = ~(df[compound_names]==0).any(axis=1)
-
-    columns = list(compound_names)+[SST,AGE,LONGITUDE,LATITUDE]
+    columns = GDGT+[SST,AGE,LONGITUDE,LATITUDE]
     df = df[columns].loc[idx]
 
     if modern:
@@ -65,112 +124,113 @@ def load_rattanasriampaipong2022(*, modern=True, return_X_y=False, as_frame=Fals
         df = df.drop([108, 110, 120])
     else:
         df = df.sort_values(AGE)
-        
+
     df = df.astype(np.float64)
-        
-    feature_columns = list(compound_names)+[AGE,LONGITUDE,LATITUDE]
-    
+
+    feature_columns = GDGT+[AGE,LONGITUDE,LATITUDE]
+
     data, target, feature_names, target_names = df[feature_columns].values, df[SST].values, feature_columns, [SST]
 
     fdescr = read_text(DESCR_MODULE, "rattanasriampaipong2022.rst")
 
     frame = None
     target_columns = [
-        SST,
-    ]
+            SST,
+            ]
     if as_frame:
         frame, data, target = _convert_data_dataframe(
-            "load_rattanasriampaipong2022", data, target, feature_names, target_columns
-        )
+                "load_rattanasriampaipong2022", data, target, feature_names, target_columns
+                )
 
     if return_X_y:
         return data, target
 
     return Bunch(
-        data=data,
-        target=target,
-        frame=frame,
-        target_names=target_names,
-        DESCR=fdescr,
-        feature_names=feature_names,
-        filename=data_file_name,
-        data_module=DATA_MODULE,
-    )
+            data=data,
+            target=target,
+            frame=frame,
+            target_names=target_names,
+            DESCR=fdescr,
+            feature_names=feature_names,
+            filename=data_file_name,
+            data_module=DATA_MODULE,
+            )
 
 
 @validate_params(
-    {"return_X_y": ["boolean"], "as_frame": ["boolean"]},
-    prefer_skip_nested_validation=True,
-)
-def load_duncan2023(*, modern=True, return_X_y=False, as_frame=False):
+        {"return_X_y": ["boolean"], "as_frame": ["boolean"]},
+        prefer_skip_nested_validation=True,
+        )
+def load_duncan2023(*, modern=True, remove_zeros=False, return_X_y=False, as_frame=False):
     data_file_name = "../data/external/GDGTdata_Antarctica_220923.xlsx"
-    
-    sst_name = "Sea Surface Temp"
-
-    # Mapping of column names
-    col_map = {
-        1302: "GDGT-0",
-        1300: "GDGT-1",
-        1298: "GDGT-2",
-        1296: "GDGT-3",
-        1292: "Crenarchaeol",
-        "1292'": "Cren'"
-        }
 
     if modern:
         df = pd.read_excel(data_file_name, sheet_name="iso modern cal" ,skiprows=1).dropna()
         df[AGE] = 0.0
 
-        compound_names = df.columns[2:8]
-
-        idx = ~(df[compound_names]==0).any(axis=1)
-        columns = list(compound_names)+[sst_name,AGE,LONGITUDE,LATITUDE]
+        idx = ~(df[GDGT]==0).any(axis=1)
+        columns = GDGT+[SST,AGE,LONGITUDE,LATITUDE]
         df[LATITUDE] = df["latitude"].values
         df[LONGITUDE] = df["longitude"].values
-        df = df[columns].loc[idx]
+        df[SST]       = df["Sea Surface Temp"].values
+        if remove_zeros:
+            df = df[columns].loc[idx]
+        else:
+            df = df[columns]
     else:
         df = pd.read_excel(data_file_name, sheet_name="Combined")
-        df[sst_name] = np.nan
-        df[LATITUDE] = df["Latitude (approx paleo)"].values
-        df[LONGITUDE] = df["Longitude (approx paleo)"].values
-        
+
+        # Mapping of column names
+        col_map = {
+                1302: "GDGT-0",
+                1300: "GDGT-1",
+                1298: "GDGT-2",
+                1296: "GDGT-3",
+                1292: "Crenarchaeol",
+                "1292'": "Cren'"
+                }
         df = df.rename(columns=col_map)
 
-        df = df.drop(1304)
-        
-        compound_names = df.columns[8:14]
+        df[SST] = np.nan
+        df[LATITUDE] = df["Latitude (approx paleo)"].values
+        df[LONGITUDE] = df["Longitude (approx paleo)"].values
 
-        idx = ~(df[compound_names]==0).any(axis=1)
-        columns = list(compound_names)+[sst_name,AGE,LONGITUDE,LATITUDE]
-        df = df[columns].loc[idx]
-        
+        df = df.drop(1304)
+
+        idx = ~(df[GDGT]==0).any(axis=1)
+        columns = GDGT+[SST,AGE,LONGITUDE,LATITUDE]
+        if remove_zeros:
+            df = df[columns].loc[idx]
+        else:
+            df = df[columns]
+
         df = df.sort_values(AGE)
-    
-    feature_columns = list(compound_names)+[AGE,LONGITUDE,LATITUDE]
-    
-    data, target, feature_names, target_names = df[feature_columns].values, df[sst_name].values, feature_columns, [SST]
+
+    feature_columns = GDGT+[AGE,LONGITUDE,LATITUDE]
+
+    data, target, feature_names, target_names = df[feature_columns].values, df[SST].values, feature_columns, [SST]
 
     fdescr = read_text(DESCR_MODULE, "duncan2023.rst")
 
     frame = None
     target_columns = [
-        SST,
-    ]
+            SST,
+            ]
     if as_frame:
         frame, data, target = _convert_data_dataframe(
-            "load_duncan2023", data, target, feature_names, target_columns
-        )
+                "load_duncan2023", data, target, feature_names, target_columns
+                )
 
     if return_X_y:
         return data, target
 
     return Bunch(
-        data=data,
-        target=target,
-        frame=frame,
-        target_names=target_names,
-        DESCR=fdescr,
-        feature_names=feature_names,
-        filename=data_file_name,
-        data_module=DATA_MODULE,
-    )
+            data=data,
+            target=target,
+            frame=frame,
+            target_names=target_names,
+            DESCR=fdescr,
+            feature_names=feature_names,
+            filename=data_file_name,
+            data_module=DATA_MODULE,
+            )
